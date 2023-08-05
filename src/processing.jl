fdec494421063cc592b7d76522cbc1aeaa610e74
Oliver Lindemann <lindemann09@googlemail.com>
2023-08-05 14:44:00 +0200

    fixes

--- src/processing.jl
+++ src/processing.jl
@@ -24,11 +24,11 @@ function epoch_rejection(fe::ForceEpochs;
 )
 	min = minimum(fe)
 	max = maximum(fe)
-	peak_differences = peak_differences(fe.dat, max_diff_windows_size)
-	good = min .>= force_range.start .||
-		  max .<= force_range.stop .||
-		  abs.(peak_differences) .<= max_difference
-	return subset(fe.design, good)
+	peak_diff = peak_differences(fe.dat, max_diff_windows_size)
+	good = min .>= force_range.start .&&
+		  max .<= force_range.stop .&&
+		  abs.(peak_diff) .<= max_difference
+	return subset(fe, good)
 end
 
 
@@ -37,14 +37,13 @@ function aggregate(
 	fe::ForceEpochs{T};
 	condition::ColumnIndex = :all,
 	subject_id::Union{Nothing, ColumnIndex} = nothing,
-	row_idx_column = :row, ### FIXME NOT REQUIRED
 	agg_fnc = mean,
 ) where T <: AbstractFloat
 
 	# aggregate per subject
 	agg_forces = Matrix{T}(undef, 0, size(fe.dat, 2))
 	agg_baseline = T[]
-	rows = fe.design[:, row_idx_column]
+
 	if condition == :all
 		conditions = repeat([true], nrow(fe.design))
 	else
@@ -53,11 +52,11 @@ function aggregate(
 	Tiv = eltype(unique(conditions)) # unique require to deal with CategoricalArrays
 
 	if isnothing(subject_id)
-		dsgn = Dict(condition => Tiv[], row_idx_column => Int64[])
+		dsgn = Dict(condition => Tiv[])
 		for cond in unique(conditions)
 			push!(dsgn[condition], cond)
 			ids = findall(conditions .== cond)
-			agg_fe = agg_fnc(fe; rows = rows[ids])
+			agg_fe = agg_fnc(fe; rows = ids)
 			agg_forces = vcat(agg_forces, agg_fe.dat)
 			append!(agg_baseline, agg_fe.baseline)
 
@@ -65,42 +64,36 @@ function aggregate(
 	else
 		subject_ids = fe.design[:, subject_id]
 		Tsid = eltype(unique(subject_ids))
-		dsgn = Dict(condition => Tiv[], subject_id => Tsid[],
-			row_idx_column => Int64[])
+		dsgn = Dict(condition => Tiv[], subject_id => Tsid[])
 		for sid in unique(subject_ids)
 			for cond in unique(conditions)
 				push!(dsgn[condition], cond)
 				push!(dsgn[subject_id], sid)
 				ids = findall(subject_ids .== sid .&& conditions .== cond)
-				agg_fe = agg_fnc(fe; rows = rows[ids])
+				agg_fe = agg_fnc(fe; rows = ids)
 				agg_forces = vcat(agg_forces, agg_fe.dat)
 				append!(agg_baseline, agg_fe.baseline)
 			end
 		end
 	end
 
-	dsgn[row_idx_column] = 1:length(dsgn[condition])
 	delete!(dsgn, :all)
 	return ForceEpochs(
 		agg_forces, fe.sr, DataFrame(dsgn), agg_baseline, fe.zero_sample)
 end;
 
-function subset(fe::ForceEpochs, rows::Base.AbstractVecOrTuple{Integer}; row_idx_column::String = "row") ## FIXME see above "row" is not required
+function subset(fe::ForceEpochs, rows::Base.AbstractVecOrTuple{Integer})
 	force = fe.dat[rows, :]
 	bsln = fe.baseline[rows]
 	subset_design = fe.design[rows, :]
-	subset_design[:, row_idx_column] = 1:nrow(subset_design) # renumber FIXME
 	return ForceEpochs(force, fe.sr, subset_design, bsln, fe.zero_sample)
 end
 
-function subset(fe::ForceEpochs, subset_design::DataFrame; row_idx_column::String = "row")
-	rows = subset_design[:, row_idx_column]
-	return subset(fe, convert(Vector{Int64}, rows); row_idx_column)
-end
 
-function subset(fe::ForceEpochs, args...; row_idx_column::String = "row")
-	df = subset(fe.design, args...)
-	rows = convert(Vector{Int64}, df[:, row_idx_column])
-	return subset(fe, rows; row_idx_column)
+function subset(fe::ForceEpochs, args...)
+	df = copy(fe.design)
+	df.row_xxx .= 1:nrow(df)
+	df = subset(df, args...)
+	return subset(fe, df[:, :row_xxx])
 end
 
